663f7bde640ede1a3f6e4a642305b490d0051c68
Iarlen Reis <iarlensr@hotmail.com>
2022-10-16 06:16:08 -0300

    Adicionando 1 linha na pasta console.log

--- 1-ConsoleLog/index.js
+++ 1-ConsoleLog/index.js
@@ -3,8 +3,8 @@
 console.log("'Iarlen' Reis"); // String = Texto
 console.log(1234); // Number = Número
 console.log(15.35) // Number = Número flutuante.
-console.log(10, 20.5, 'banana') // Aceita todos tipos de dados.
-
+console.log(10, 20.5, 'banana'); // Aceita todos tipos de dados.
+console.log('Apenas um teste');
 
 // Praticando o que aprendi:
 console.log('Meu nome é "IARLEN." Estou aprendendo JavaScript ás', 04, 'dá manhã.')
